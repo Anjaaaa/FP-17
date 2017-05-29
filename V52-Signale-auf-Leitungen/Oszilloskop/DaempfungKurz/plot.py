48f98653ea7943131b6ec595e5c7c327d6e214b1
Clara Rittmann <clara.rittmann@googlemail.com>
2017-05-29 14:07:14 +0200

    Daten auslesen

--- V52-Signale-auf-Leitungen/Oszilloskop/DaempfungKurz/plot.py
+++ V52-Signale-auf-Leitungen/Oszilloskop/DaempfungKurz/plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x, y = np.genfromtxt('F0054CH1.txt', unpack = True)
+x, y = np.genfromtxt('F0041MTH.txt', unpack = True)
 
 plt.plot(x, y, 'bo', ms = 1)
 plt.savefig('F0054CH1.pdf')
--- V52-Signale-auf-Leitungen/Oszilloskop/DaempfungLang/plot.py
+++ V52-Signale-auf-Leitungen/Oszilloskop/DaempfungLang/plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x, y = np.genfromtxt('F0054CH1.txt', unpack = True)
+x, y = np.genfromtxt('F0042MTH.txt', unpack = True)
 
 plt.plot(x, y, 'bo', ms = 1)
 plt.savefig('F0054CH1.pdf')
